464f53ea944598ca64b11420ae2df81621c3df09
Yuvette <Yuvette@FLORA>
2016-11-04 13:13:20 -0500

    cleared daos

--- src/GST_Badge_System/DAO/BadgeDAO.cs
+++ src/GST_Badge_System/DAO/BadgeDAO.cs
@@ -14,61 +14,7 @@ namespace GST_Badge_System.DAO
     public class BadgeDAO
     {
 
-        //Badge
-        public static void Add_Badge(Badge new_Badge)
-        {
-            using (IDbConnection db = new SqlConnection(@"Data Source=.\SQLEXPRESS; Initial Catalog = BadgeSystemSQLDB; Integrated Security = True; Connect Timeout = 15; Encrypt = False; TrustServerCertificate = True; ApplicationIntent = ReadWrite; MultiSubnetFailover = False"))
-            {
-                string SqlQuery = @"INSERT INTO Badge (Badge_Image , Badge_RetireDate ,Badge_ActivateDate ,Badge_ID ,Badge_Name,Badge_Descript ,Badge_Status,BT_ID ,BGT_ID) VALUES (@Badge_Image , @Badge_RetireDate ,@Badge_ActivateDate ,@Badge_ID ,@Badge_Name,@Badge_Descript ,@Badge_Status,@BT_ID ,@BGT_ID)";
-                db.Execute(SqlQuery, new_Badge);
-
-            }
-        }
-        public static void Delete_Badge(Badge badge)
-        {
-            using (IDbConnection db = new SqlConnection(@"Data Source=.\SQLEXPRESS; Initial Catalog = BadgeSystemSQLDB; Integrated Security = True; Connect Timeout = 15; Encrypt = False; TrustServerCertificate = True; ApplicationIntent = ReadWrite; MultiSubnetFailover = False"))
-            {
-                string SqlQuery = "DELETE FROM Badge WHERE Badge_ID = @Badge_ID ";
-                db.Execute(SqlQuery, badge);
-            }
-        }
-
-        public static void Retire_Badge(Badge badge)
-        {
-            using (IDbConnection db = new SqlConnection(@"Data Source=.\SQLEXPRESS; Initial Catalog = BadgeSystemSQLDB; Integrated Security = True; Connect Timeout = 15; Encrypt = False; TrustServerCertificate = True; ApplicationIntent = ReadWrite; MultiSubnetFailover = False"))
-            {
-                string SqlQuery = " ";
-            }
-        }
-        public static void Update_Badge(Badge badge)
-        {
-            using (IDbConnection db = new SqlConnection(@"Data Source=.\SQLEXPRESS; Initial Catalog = BadgeSystemSQLDB; Integrated Security = True; Connect Timeout = 15; Encrypt = False; TrustServerCertificate = True; ApplicationIntent = ReadWrite; MultiSubnetFailover = False"))
-            {
-                string SqlQuery = "UPDATE Badge SET Badge_Image = @Badge_Image," + " Badge_RetireDate = @Badge_RetireDate,"+ "Badge_ActivateDate = @Badge_ActivateDate ," +"Badge_Name = @Badge_Name ,"+ "Badge_Descript = @Badge_Descript ," + "Badge_Status =@Badge_Status ," +"BadgeType = @Badge_Type ," +"BadgeGiveType = @BadgeGiveType " +"WHERE Badge_ID = @Badge_ID ";
-                db.Execute(SqlQuery,badge);
-            }
-        }
-        public static Badge Find_Badge(int id)
-        {
-            using (IDbConnection db = new SqlConnection(@"Data Source=.\SQLEXPRESS; Initial Catalog = BadgeSystemSQLDB; Integrated Security = True; Connect Timeout = 15; Encrypt = False; TrustServerCertificate = True; ApplicationIntent = ReadWrite; MultiSubnetFailover = False"))
-            {
-
-                return db.Query<Badge>("Select * From Badge" +
-
-                 "WHERE Badge_ID = @Badge_ID", new { id }).SingleOrDefault();
-
-            }
-        }
-        public static create_Badge_DB (string filePath)
-        {
-            //Delete current db
-            //create new db
-            // read file & insert badges
-        }
-
-        //BadgeType
-
-
+  
 
     }
 }
--- src/GST_Badge_System/DAO/UserDAO.cs
+++ src/GST_Badge_System/DAO/UserDAO.cs
@@ -14,53 +14,7 @@ namespace GST_Badge_System.DAO
      */
     public class UserDAO
     {
-        public static void Add_User(User new_User)
-        {
-            using (IDbConnection db = new SqlConnection(@"Data Source=.\SQLEXPRESS; Initial Catalog = BadgeSystemSQLDB; Integrated Security = True; Connect Timeout = 15; Encrypt = False; TrustServerCertificate = True; ApplicationIntent = ReadWrite; MultiSubnetFailover = False"))
-            {
-                string SqlQuery = @"INSERT INTO USERS (User_ID ,User_NAME ,EMAIL ,USER_TYPE ) VALUES (@User_ID ,@User_NAME ,@EMAIL ,@USER_TYPE)";
-                db.Execute(SqlQuery, new_User);
 
-            }
-        }
-        public static void Delete_User(User user)
-        {
-            using (IDbConnection db = new SqlConnection(@"Data Source=.\SQLEXPRESS; Initial Catalog = BadgeSystemSQLDB; Integrated Security = True; Connect Timeout = 15; Encrypt = False; TrustServerCertificate = True; ApplicationIntent = ReadWrite; MultiSubnetFailover = False"))
-            {
-                string SqlQuery = "DELETE FROM Badge WHERE User_ID = @User_ID ";
-                db.Execute(SqlQuery, user);
-            }
-        }
-
-     
-        public static void Update_User(User user)
-        {
-            using (IDbConnection db = new SqlConnection(@"Data Source=.\SQLEXPRESS; Initial Catalog = BadgeSystemSQLDB; Integrated Security = True; Connect Timeout = 15; Encrypt = False; TrustServerCertificate = True; ApplicationIntent = ReadWrite; MultiSubnetFailover = False"))
-            {
-                string SqlQuery = "UPDATE Users SET User_NAME = @User_NAME,"+"EMAIL= @EMAIL ,"+"USER_TYPE =@USER_TYPE" + "WHERE User_ID = @User_ID ";
-                db.Execute(SqlQuery, user);
-            }
-        }
-        public static Badge Find_Badge(int id)
-        {
-            using (IDbConnection db = new SqlConnection(@"Data Source=.\SQLEXPRESS; Initial Catalog = BadgeSystemSQLDB; Integrated Security = True; Connect Timeout = 15; Encrypt = False; TrustServerCertificate = True; ApplicationIntent = ReadWrite; MultiSubnetFailover = False"))
-            {
-
-                return db.Query<User>("Select * From USERS" +
-
-                 "WHERE User_ID = @User_ID", new { id }).SingleOrDefault();
-
-            }
-        }
-        public static create_User_DB(string filePath)
-        {
-            using (IDbConnection db = new SqlConnection(@"Data Source=.\SQLEXPRESS; Initial Catalog = BadgeSystemSQLDB; Integrated Security = True; Connect Timeout = 15; Encrypt = False; TrustServerCertificate = True; ApplicationIntent = ReadWrite; MultiSubnetFailover = False"))
-            {
-                //Delete current db
-                foreach (User u in db)
-            //create new db
-            // read file & insert Users from file
-        }
-
+       
     }
 }
